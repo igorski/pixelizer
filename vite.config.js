f4f0d2378741bbd20a5d1a9c1279ead8334fa70f
Igor Zinken <igor.zinken@gmail.com>
2024-07-22 19:28:40 +0200

    Initial config

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import path from "path";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    base: "./",
+    plugins: [
+        vue(),
+    ],
+    resolve: {
+        alias: {
+            "@": path.resolve( __dirname, "./src" ),
+        },
+    },
+});
