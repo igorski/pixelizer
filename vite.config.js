6f6abfa223ebdaa87928b1fefcd131929fac0874
Igor Zinken <igor.zinken@gmail.com>
2024-08-02 22:15:40 +0200

    Added logos

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,22 @@
 import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 import path from "path";
 import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
+const dirSrc    = `./src`;
+const dirAssets = `./assets`;
+const dest      = `${__dirname}/dist`;
+
 export default defineConfig({
     base: "./",
     plugins: [
         vue(),
+        viteStaticCopy({
+            targets: [{
+                src: dirAssets,
+                dest: path.resolve( dest ),
+            }]
+        }),
     ],
     resolve: {
         alias: {
